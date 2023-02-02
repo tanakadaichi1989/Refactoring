64166d06c52a23dbfa6252f655716e1a680832ed
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2023-02-02 13:19:35 +0900

    Add codes regarding Price and Refactor codes

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("This code is sample for Refactoring.");
-
         Customer customer = new Customer("Customer1");
+
         Movie movie1 = new Movie("Movie1", 0);
+        Movie movie2 = new Movie("Movie2", 1);
+        Movie movie3 = new Movie("Movie3", 2);
+
         Rental rental1 = new Rental(movie1, 14);
+        Rental rental2 = new Rental(movie2, 14);
+        Rental rental3 = new Rental(movie3, 14);
 
         customer.addRental(rental1);
+        customer.addRental(rental2);
+        customer.addRental(rental3);
+        
         System.out.println(customer.statement());
     }
 }
