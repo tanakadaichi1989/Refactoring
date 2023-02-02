71384e51db9a841ec856dd6b21e35cd84a65da3c
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2023-02-02 09:45:13 +0900

    Add sample code in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("This code is sample for Refactoring.");
+
+        Customer customer = new Customer("Customer1");
+        Movie movie1 = new Movie("Movie1", 0);
+        Rental rental1 = new Rental(movie1, 14);
+
+        customer.addRental(rental1);
+        System.out.println(customer.statement());
     }
 }
